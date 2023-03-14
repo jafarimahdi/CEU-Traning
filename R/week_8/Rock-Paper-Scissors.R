270dee648cce74863a6865a723fe81dc26627e25
jafarimahdi <mahdijafarii.mj@gmail.com>
2023-03-14 19:55:53 +0100

    Update Rock-Paper-Scissors.R

--- R/week_8/Rock-Paper-Scissors.R
+++ R/week_8/Rock-Paper-Scissors.R
@@ -1,12 +1,10 @@
- # Rock, Paper, Scissors game 
-
-
+# Rock, Paper, Scissors game 
 
 winner <-function(){
-your_Score <- 0
-machine_Score <- 0
-  
-  while (your_Score < 2 || machine_Score < 2) {
+  your_Score <- 0
+  machine_Score <- 0
+  gameOn <- TRUE
+  while (gameOn) {
     
     userInput <-  as.integer(readline("choice a number \nrock= 1, \npaper=2, \nscissors=3 \nwhat is your shot?"))
     
@@ -15,42 +13,33 @@ machine_Score <- 0
     result <- ""
     
     if(userInput == 1 && thirdUser == 1 ){result = "draw"}
-    if(userInput == 1 && thirdUser == 2 ){result ="You lose"}
+    if(userInput == 1 && thirdUser == 2 ){result = "You lose"}
     if(userInput == 1 && thirdUser == 3){result ="You win"} 
     if(userInput == 2 && thirdUser == 1){ result ="You win"}
     if(userInput == 2 && thirdUser == 2){ result = "draw"}
-    if(userInput == 2 && thirdUser == 3){ result ="You lose"}
-    if(userInput == 3 && thirdUser == 1){ result ="You lose"}
+    if(userInput == 2 && thirdUser == 3){ result = "You lose"}
+    if(userInput == 3 && thirdUser == 1){ result = "You lose"}
     if(userInput == 3 && thirdUser == 2){ result ="You win"}
     if(userInput == 3 && thirdUser == 3){ result ="draw"}
-  else{
-      result ="plase Enter correct answer to the concil "
-    }
     
     if(result == "You win") {your_Score = your_Score +1}
     if(result == "You lose"){ machine_Score = machine_Score +1}
-  
+    
     print(result)
     cat("your score:", your_Score, " machine Score:", machine_Score, " \n")
     
     if(your_Score == 3){
       print("you won the 3 games")
-      break
+      gameOn <- FALSE
     }else if(machine_Score == 3){
       print("you lost bro")
-      break
+      gameOn <- FALSE
     }}
 }
 
 
 winner()
 
-
-#list <- c("rock","paper", "scissors")
-#cat("your choice was: ", list[userInput] ,"\nmachine choice was: ", list[thirdUser],"\nand you: " , result)
-
-
- 
 
 
 
