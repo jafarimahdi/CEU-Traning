ea92dc1ddf0c7b9816c3cb6912ec28b7c7220a97
jafarimahdi <mahdijafarii.mj@gmail.com>
2023-03-14 19:56:05 +0100

    Update Guess-me.R

--- R/week_8/Guess-me.R
+++ R/week_8/Guess-me.R
@@ -3,7 +3,7 @@
 # to be higher or lower 
 
 
-guesst_me <-function(){
+guess_me <-function(){
   thirdUser <- sample(1:100, 1)         
   your_Score <- 5
   
@@ -17,11 +17,44 @@ guesst_me <-function(){
     else if(userInput < thirdUser){result ="Your Number is lower "} 
     else{ result = "pleae Enter the correct input "}
     
-    cat(result, "and you have ", your_Score, " Gueses left ")
+    print(paste(result, "and you have ", your_Score, " Gueses left "))
     
     your_Score <- your_Score -1
   }
 }
 
 
-print(guesst_me())
+guess_me()
+
+
+# Second Game ____________________________________________
+
+# the input ask from user 2 input, what should whit max number an how many guess you want from this game 
+
+guess <-function(){
+  maxNum <-  as.integer(readline("what should be the max Num? : "))
+  your_Score <-  as.integer(readline("How many guess you want? : "))
+  
+  thirdUser <- sample(1:maxNum, 1)         
+  
+  
+  while (your_Score >= 0) {
+    userInput <-  as.integer(readline("what is your next guess? : "))
+    result <- ""
+    
+    if(userInput == thirdUser ){result ="Yes, You found it "}
+    else if(userInput > thirdUser){result ="Your Number is highr "}
+    else if(userInput < thirdUser){result ="Your Number is lower "} 
+    else{ result = "pleae Enter the correct input "}
+    
+    print(paste(result, "and you have ", your_Score, " Gueses left "))
+    
+    your_Score <- your_Score -1
+  }
+}
+
+guess()
+
+
+
+
