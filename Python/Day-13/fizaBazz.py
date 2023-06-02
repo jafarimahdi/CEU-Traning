71bcc6c49cd37b46ccf0646c848514caa6fd1314
jafarimahdi <mahdijafarii.mj@gmail.com>
2023-06-03 00:19:08 +0200

    day 13

---
+++ Python/Day-13/fizaBazz.py
@@ -0,0 +1,24 @@
+# Instructions
+# Read the code below
+# Spot the problems 🐞.
+# Modify the code to fix the program.
+
+# for number in range(1, 100):
+#   if number % 3 == 0 or number % 5 == 0:
+#     print("FizzBuzz")
+#   elif number % 3 == 0:
+#     print("Fizz")
+#   elif number % 5 == 0:
+#     print("Buzz")
+#   else:
+#     print(number)
+
+for number in range(1, 101):
+  if number % 3 == 0 and number % 5 == 0:
+    print("FizzBuzz")
+  elif number % 3 == 0:
+    print("Fizz")
+  elif number % 5 == 0:
+    print("Buzz")
+  else:
+    print(number)
