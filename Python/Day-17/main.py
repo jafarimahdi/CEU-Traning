143ca9fdbfad7c121b11335df3624877b141f4fc
Mate <mahdijafarii.mj@gmail.com>
2023-09-04 01:11:07 +0200

    class & constructor

---
+++ Python/Day-17/main.py
@@ -0,0 +1,25 @@
+
+class User:
+    def __init__(self, user_id: str, username: str):
+        self.id = user_id
+        self.username = username
+        self.followers = 0
+        self.following = 0
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
+
+
+
+user_1 = User("001", "max")
+user_2 = User("002", 'mahdi')
+
+print(user_1.id, user_1.username, user_1.followers)
+
+
+user_1.follow(user_2)
+
+print(user_1.followers)
+print(user_1.following)
+print(user_2.followers)
+print(user_2.following)
