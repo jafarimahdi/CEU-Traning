f97f051fb22bc2988405dcf96fecea90f9c77945
someOne <mahdijafarii.mj2@gmail.com>
2023-11-13 10:59:33 +0100

    add score class

--- Python/Day-20_21/snake_game/main.py
+++ Python/Day-20_21/snake_game/main.py
@@ -2,6 +2,8 @@ from turtle import Screen
 import time
 from snake import Snake
 from food import Food
+from scoreBoard import Score
+
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -11,6 +13,8 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
+score = Score()
+
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -28,6 +32,7 @@ while is_game_on:
 # get the food
     if snake.head.distance(food) < 15:
         food.refresh_food()
+        score.increase()
 
 
 screen.exitonclick()
---
+++ Python/Day-20_21/snake_game/scoreBoard.py
@@ -0,0 +1,25 @@
+from turtle import Turtle
+ALIGN = "center"
+FONT = ("Arial", 8, "normal")
+
+
+class Score(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.goto(0, 270)
+        self.hideturtle()
+        self.update_score()
+
+    def update_score(self):
+        self.write(f"Score:{self.score}", align=ALIGN, font=FONT)
+
+    def increase(self):
+        self.score += 1
+        self.clear()
+        self.write(f"Score:{self.score}", align=ALIGN, font=FONT)
+
+
+
