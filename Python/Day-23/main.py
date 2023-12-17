192febd747610d4a96b95db74d08c0ce9d1ca2bb
jafarimahdi <mahdijafarii.mj@gmail.com>
2023-12-17 23:38:19 +0100

    Detect turtle collides with cars

--- Python/Day-23/main.py
+++ Python/Day-23/main.py
@@ -22,3 +22,11 @@ while game_is_on:
 
     car_manager.create_car()
     car_manager.move_car()
+
+    # check if the cars hit turtle
+    for item in car_manager.all_cars:
+        if item.distance(player) < 20:
+            game_is_on = False
+
+
+screen.exitonclick()
